cd661976ec7b976eedccac9664672f00a08b15d1
Eric-0221-yzc <3572747575@qq.com>
2026-04-16 12:52:41 +0800

    Update borrow_system.py

--- Task 1/borrow_system.py
+++ Task 1/borrow_system.py
@@ -1,21 +1,27 @@
 class BorrowSystem:
+    """Core logic for campus item borrowing system"""
     def __init__(self):
-        self.items = {}
-        self.users = {}
-        self.records = []
+        self.items = {}  # Store items: {item_id: Item object}
+        self.users = {}  # Store users: {user_id: User object}
+        self.records = [] # Borrow/return history
 
+    # Add a new item
     def add_item(self, item):
         self.items[item.get_item_id()] = item
 
+    # Add a new user
     def add_user(self, user):
         self.users[user.get_user_id()] = user
 
+    # Get item by ID
     def get_user(self, user_id):
         return self.users.get(user_id)
-    
+
+    # Get item by ID
     def get_item(self, item_id):
         return self.items.get(item_id)
-    
+
+    # Borrow an item
     def borrow_item(self, user_id, item_id):
         user = self.users.get(user_id)
         item = self.items.get(item_id)
@@ -25,7 +31,8 @@ class BorrowSystem:
             self.records.append(f"{user_id} borrow {item_id}")
             return True, "Borrowing Successfully"
         return False, "Item has been borrowed"
-    
+
+    # Return an item
     def return_item(self, item_id):
         item = self.get_item(item_id)
         if not item:
@@ -34,9 +41,11 @@ class BorrowSystem:
             self.records.append(f"{item_id} Item returned")
             return True, "Return Successfully"
         return False, "Item was not lent out"
-    
+
+    # Show all items 
     def show_all_items(self):
         return list(self.items.values())
-    
+
+    # Show all operation records
     def show_records(self):
         return self.records
